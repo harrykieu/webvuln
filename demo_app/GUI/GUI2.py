a314addbe8068ad65f1893fb0f1e4b30111c517a
harrykieu <huyhai321@gmail.com>
2023-09-10 23:55:14 +0700

    Add demo

--- demo_app/GUI/GUI2.py
+++ demo_app/GUI/GUI2.py
@@ -1,8 +1,6 @@
 import flet
-from flet import Page, TextField, TextButton,Text,colors,Row,TextStyle,InputBorder,Container,Column
-from io import BytesIO
-import json
-from main import main as scanner
+from flet import Page, TextField, Image ,Text,colors,TextStyle,InputBorder,Container, FilledButton
+import time
 import subprocess
 
 def main(page: Page):
@@ -15,7 +13,6 @@ def main(page: Page):
         "Montserrat":"https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;1,300;1,400&display=swap"
     }
 
-
     def getCmd(e):
         try:
             result = subprocess.check_output({e}, shell=True, text=True)
@@ -25,29 +22,34 @@ def main(page: Page):
             print(f"Error: {e}")
             return "Can't run the cmd line"
         
-    
-# doi lai e items
-# nghien cuu pyc convert python -> C
-# tao file requirement.txt cho ae cai library
-
     def on_changed_text(e):
         result.value = getCmd(e.control.value)
         getCmd(e.control.value)
         print(e.control.value)
         page.update()
 
-    def scan():
-        url_to_scan = input_url.control.value
-        result.value = scanner(url_to_scan)
-        print(result.value)
+    def run_graph(e):
+        
+        try:
+            process = subprocess.run("python .\demo_app\GUI\graph_test.py", shell=True, text=True)
+            if process.returncode == 0:
+                print("Success")
+                image.src = './demo_app/GUI/asset/graph.png'
+                page.update(image)
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+            return "Can't run the graph"
+# Duration??
 
-    input_url = TextField(label='Input url',width=500,height=70,color=colors.BLACK,label_style=TextStyle(color=colors.BLACK),border_color=colors.BLACK,on_submit=scan)
     container = Container(height='30px')
-
+    imageButton = FilledButton('Run demo graph',on_click=run_graph)
     result = Text(color=colors.BLACK,font_family='Montserrat',size=20)
     cmd = TextField(label='Enter the command line CMD and press Enter',width=500,height=70,on_submit=on_changed_text,bgcolor=colors.GREY_500,label_style=TextStyle(color=colors.BLACK,font_family='Arial'),color=colors.BLACK,border=InputBorder.UNDERLINE,filled=True,prefix_text="/>>",prefix_style=TextStyle(color=colors.BLACK))
+    image = Image(src='./demo_app/GUI/asset/cat.jpg',width=500,height=500,fit=flet.ImageFit.CONTAIN)
+
     # run = TextButton(content='Run')
-    page.add(cmd,input_url,container,result)
+    page.add(cmd,container,result, imageButton,image)
     page.update()
 
 flet.app(target=main, assets_dir="./assets/fonts")
+
