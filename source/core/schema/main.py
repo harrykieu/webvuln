1bb6dcae512e041936af17b8464b9223ad724e5a
vdh1612 <125654739+vdh1612@users.noreply.github.com>
2023-09-14 11:02:03 +0700

    schema

---
+++ source/core/schema/main.py
@@ -0,0 +1,64 @@
+from pymongo import MongoClient 
+from bson.objectid import ObjectId
+from datetime import datetime, timezone
+from collections import OrderedDict
+import sys
+import json
+
+client = MongoClient("mongodb://localhost:27017/webvuln")
+
+db = client.webvuln
+db.web_collection.drop()
+db.create_collection("web_collection")
+
+
+# create new documents
+web1 = {
+     '_id': ObjectId(),
+    'website': "example.com",
+    'date' : datetime.now(tz=timezone.utc),
+    'num_vul': 0,
+    'vulnerability': [
+    ],
+    'severity': 'Low'
+}
+web2 = {
+    '_id': ObjectId(),
+    'website': "example2.com",
+    'date' : datetime.now(tz=timezone.utc),
+    'num_vul': 0,
+    'vulnerability': [
+    ],
+    'severity': "Low"
+}
+
+
+webs = []
+webs.append(web1)
+webs.append(web2)
+
+with open('schema\\web_vuln.json','r') as j:
+    vexpr = json.load(j)
+
+# assign the schema validation expression to the cmd variable
+cmd = OrderedDict([
+    ('collMod','web_collection'),
+    ('validator', vexpr),
+    ('validationLevel','moderate')
+])
+
+
+try:
+    db.command(cmd)
+    print("Check whether valdiation works: All good")
+except Exception as e:
+    print("exc",e)
+
+web_collection = db["web_collection"]
+
+try:
+    for i in range(len(webs)):
+        web_collection.insert_one(webs[i])
+        print(f"Insert document {i+1} successfully !")
+except:
+    print("exc",sys.exc_info())
