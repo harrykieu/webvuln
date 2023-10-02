e757b875d4811794a9415c9b31c5bfd6a1c52d89
vdh1612 <125654739+vdh1612@users.noreply.github.com>
2023-10-02 08:47:04 +0700

    database

--- source/core/database/database.py
+++ source/core/database/database.py
@@ -1,17 +1,18 @@
 from pymongo import MongoClient 
-from bson.objectid import ObjectId
 from collections import OrderedDict
 import json
 import  utils 
 import os
 from isodate import parse_datetime 
+
+
 client = MongoClient(os.getenv("DATABASE_URI"))
 db = client.webvuln
+database_utils = utils.Database_utils()
 
 
 db.scanResult.drop()
 db.create_collection("scanResult")
-database_utils_scan = utils.Database_utils_scan()
 scanResult = db["scanResult"]
 
 
@@ -33,18 +34,19 @@ except Exception as e:
     print("exc",e)
 
 
-
 with open(f'{__file__}\\..\\data_scanResult.json') as file:
-        file_data = json.load(file)
-
-database_utils_scan.add_document("scanResult",file_data)
+        scanResult_data = json.load(file)
+        
+for document in scanResult_data:
+    document["scanDate"] = parse_datetime(document["scanDate"])
+database_utils.add_document("scanResult",scanResult_data)
 
 
 # document = database_utils.find_document("scanResult", {})
 # print(list(document))
 
 
-# database_utils.delete_document("domain", {"domain":"example.com"})
+# database_utils.delete_document("scanResult", {"domain":"example.com"})
 
 
 # database_utils.edit_document("scanResult", {"domain":"example2.com"},{"$set":{"domain":"example4.com"}})
@@ -53,7 +55,6 @@ database_utils_scan.add_document("scanResult",file_data)
 
 db.resources.drop()
 db.create_collection("resources")
-database_utils_resources = utils.Database_utils_resources()
 resources = db["resources"]
 
 with open(f'{__file__}\\..\\resources.json','r') as j:
@@ -66,15 +67,21 @@ cmd = OrderedDict([
     ("validationLevel", "moderate")
 ])
 
+
 try:
     db.command(cmd)
     print("\nCheck whether validation works for resources: All good")
 except Exception as e:
     print("exc", e)
 
+
 with open(f'{__file__}\\..\\data_resources.json') as file:
-        file_data_1 = json.load(file)
-database_utils_resources.add_document("resources",file_data_1)    
+        resources_data = json.load(file)
+        
+for document in resources_data:
+    document["createdDate"] = parse_datetime(document["createdDate"])
+    document["editedDate"] = parse_datetime(document["editedDate"])
+database_utils.add_document("resources",resources_data)    
     
 # Disconnect from the database    
-database_utils_resources.disconnect()
+database_utils.disconnect()
--- source/core/database/utils.py
+++ source/core/database/utils.py
@@ -1,10 +1,7 @@
 from pymongo import MongoClient 
-from pymongo import MongoClient
-from datetime import datetime 
-import sys
 import os
-from isodate import parse_datetime 
-class Database_utils_scan:
+
+class Database_utils:
     def __init__(self):
         self.client = MongoClient(os.getenv("DATABASE_URI"))
         self.db = self.client.get_database("webvuln")
@@ -12,93 +9,39 @@ class Database_utils_scan:
     def disconnect(self):
         self.client.close()
 
-    def add_document(self, collection_name,scanResult_data):
+    def add_document(self, collection_name,data):
         try:
             collection = self.db.get_collection(collection_name)
-            for document in scanResult_data:
-                try:
-                    document["scanDate"] = parse_datetime(document["scanDate"])
-                except  Exception as e:
-                    print("exc", e)
-            if isinstance(scanResult_data,list):
-                collection.insert_many(scanResult_data)
+            if isinstance(data,list):
+                collection.insert_many(data)
                 print("Insert documents successfully!")
             else:
-                collection.insert_one(scanResult_data)
-                print("Insert document successfully!")
-        except:
-            print("exc",sys.exc_info())   
-            
-    def delete_document(self, collection_name, query):
-        try:
-            collection = self.db.get_collection(collection_name)
-            collection.delete_one(query)
-            print("delete document successfully !")
-        except:
-            print("exc",sys.exc_info()) 
-            
-    def edit_document(self, collection_name, query, update):
-        try:
-            collection = self.db.get_collection(collection_name)
-            collection.update_one(query, update)
-            print("edit document successfully !")
-        except:
-            print("exc",sys.exc_info()) 
-            
-    def find_document(self, collection_name, query):
-        try: 
-            collection = self.db.get_collection(collection_name)
-            return collection.find(query)
-        except:
-            print("exc",sys.exc_info()) 
-
-
-class Database_utils_resources:
-    def __init__(self):
-        self.client = MongoClient(os.getenv("DATABASE_URI"))
-        self.db = self.client.get_database("webvuln")
-
-    def disconnect(self):
-        self.client.close()
-
-    def add_document(self, collection_name,resources_data):
-        try:
-            collection = self.db.get_collection(collection_name)
-            for document in resources_data:
-                try:             
-                    document["createdDate"] = parse_datetime(document["createdDate"])
-                    document["editedDate"] = parse_datetime(document["editedDate"])
-                except Exception as e:
-                    print("exc", e)
-
-            if isinstance(resources_data,list):
-                collection.insert_many(resources_data)
-                print("Insert documents successfully!")
-            else:
-                collection.insert_one(resources_data)
-                print("Insert document successfully!")
+                collection.insert_one(data)
+                print("Insert one document successfully!")
         except Exception as e:
-            print("exc",e)
+            print("exc",e)   
             
     def delete_document(self, collection_name, query):
         try:
             collection = self.db.get_collection(collection_name)
             collection.delete_one(query)
             print("delete document successfully !")
-        except:
-            print("exc",sys.exc_info()) 
+        except Exception as e:
+            print("exc",e) 
             
     def edit_document(self, collection_name, query, update):
         try:
             collection = self.db.get_collection(collection_name)
             collection.update_one(query, update)
             print("edit document successfully !")
-        except:
-            print("exc",sys.exc_info()) 
+        except Exception as e:
+            print("exc",e) 
             
     def find_document(self, collection_name, query):
         try: 
             collection = self.db.get_collection(collection_name)
             return collection.find(query)
-        except:
-            print("exc",sys.exc_info()) 
+        except Exception as e:
+            print("exc",e) 
+
+
