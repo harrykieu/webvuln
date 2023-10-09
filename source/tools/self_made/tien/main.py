80f44b92c38130b7148f18d3e91a2e4be7ae18b7
rumblelinux <rumblett70506@gmail.com>
2023-10-09 13:31:42 +0700

    rumble scan owasp 3/10

--- source/tools/self_made/tien/main.py
+++ source/tools/self_made/tien/main.py
@@ -1,44 +1,32 @@
-import argparse
+import SQLi_scan
+import XSS_scan
+import LFi_scan
+import sys
 import json
-from scanner import scan_website
-
-def generate_report(results):
-
-  report = {
-    "url_scanned": "",  
-    "vulnerabilities": []
-  }
-  
-  if results:
-    report["url_scanned"] = results[0]["url"]
-
-  for item in results:
-    report["vulnerabilities"].append({
-      "url": item["url"],
-      "vuln_type": item["vuln"]
-    })
-
-  # Lưu thành file JSON
-  with open("report.json", "w") as f: 
-    json.dump(report, f, indent=2)
-
-  return report
-
-def main():
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-u', '--url', required=True, help='URL to scan')
-  args = parser.parse_args()
-  url = args.url
-
-  results = scan_website(url)
-
-  print(f"Đã hoàn thành scan {url}, tìm thấy {len(results)} lỗ hổng")
-
-  report = generate_report(results)
-
-  print("Báo cáo đã được tạo thành công")
-
 
 if __name__ == "__main__":
-  main()
+
+  if len(sys.argv) != 2:
+      print("Usage: python main.py <url>")
+      sys.exit(1)
+
+  url = sys.argv[1]
+  print(f"Starting scan on {url}")
+  
+  sqli_result = SQLi_scan.check_sqli(url)  
+  xss_result = XSS_scan.check_xss(url)
+  lfi_result = LFi_scan.check_lfi(url)
+
+
+  # Save result to file report.json
+  report = {
+      "target": url,
+      "SQLi vulnerabilities": sqli_result,
+      "XSS vulnerabilities": xss_result,
+      "LFI vulnerabilities" : lfi_result
+  }
+  
+  with open("report.json", "w") as f:
+      json.dump(report, f, indent=4)
+
+  print("[v] Scan completed, report generated")
