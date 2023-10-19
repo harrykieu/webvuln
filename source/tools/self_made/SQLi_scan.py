7a1c281f7c354e7914c32e199ba5f7e96b9fd85f
rumblelinux <rumblett70506@gmail.com>
2023-10-19 23:43:21 +0700

    Webvul owasp top 10

--- source/tools/self_made/SQLi_scan.py
+++ source/tools/self_made/SQLi_scan.py
@@ -11,7 +11,7 @@ import time
 
 s = requests.Session()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/117.0.5938.92"
-class SQli_check:
+class SQli:
  
   def __init__(self, url):
      self.url = url
@@ -33,7 +33,7 @@ class SQli_check:
 
 
   # ---------------------------------------------------------------
-  #DEF SCAN WEBSITE
+
   forms = []
   def scan_website(self, url):
     soup = bs(s.get(url).content, "html.parser")
@@ -43,8 +43,6 @@ class SQli_check:
     urls = urlparse(url)
     global forms
     forms = bs(s.get(url).content, "html.parser").find_all("form")
-
-    # Scan SQLi
     if self.check_sqli(url):
       results["sqli"].append({
         "url": url,
@@ -54,9 +52,8 @@ class SQli_check:
 
   # -----------------------------------------------------
 
-  # Check SQLi 
   def is_vulnerable_sqli(self, response):
-    # Check normal error
+
     errors = {
       "you have an error in your sql syntax;",
       "warning: mysql",
@@ -64,19 +61,16 @@ class SQli_check:
       "quoted string not properly terminated",
     }
 
-    # Checking time response
     response_time = response.elapsed.total_seconds()
     if response_time > 10:
       print("[!] Slow response time detected")
       return True 
 
-    # Trying dump complex payload
     complex_payload = "1' AND (SELECT * FROM information_schema.tables)='"
     if complex_payload in response.content.decode().lower():
       print("[!] Complex payload was successful")
       return True
 
-    # Try dump normal payload
     dump_payload = f"1' UNION SELECT NULL, NULL, @@version;--"
     if dump_payload in response.content.decode().lower():
       print("[!] Data dump was successful") 
@@ -88,27 +82,21 @@ class SQli_check:
     
     return False
 
-  # ---------
-
   # ---------------------------------
-  # Get all form from page
+
   def get_all_forms(self, url):
     soup = bs(s.get(url).content, "html.parser")
     return soup.find_all("form")
 
-  # Get form details
   def get_form_details(self, form):
     details = {}
-    # Get the form action (target url)
     try:
         action = form.attrs.get("action").lower()
     except:
         action = None
 
-    # get the form method (POST, GET, etc.)
     method = form.attrs.get("method", "get").lower()
 
-    # get all the input details such as type and name
     inputs = []
     for input_tag in form.find_all("input"):
         input_type = input_tag.attrs.get("type", "text")
@@ -116,7 +104,6 @@ class SQli_check:
         input_value = input_tag.attrs.get("value", "")
         inputs.append({"type": input_type, "name": input_name, "value": input_value})
 
-    # put everything to the resulting dictionary
     details["action"] = action
     details["method"] = method
     details["inputs"] = inputs
@@ -136,39 +123,28 @@ class SQli_check:
       new_url = f"{url}?id={encoded_payload}"
       print("[!] Trying", new_url)
 
-      # make the HTTP request
       res = s.get(new_url)
       if self.is_vulnerable_sqli(res):
-          # SQL Injection detected on the URL itself, 
-          # no need to preceed for extracting forms and submitting them
           print("[+] SQL Injection vulnerability detected, link:", new_url)
           return True
 
-
-    # test on HTML forms
     forms = self.get_all_forms(url)
     print(f"[+] Detected {len(forms)} forms on {url} and form found: {forms}")
     
     for form in forms:
         form_details = self.get_form_details(form)
         for payload in sqli_payloads:
-
-            # the data body we want to submit
             data = {}
 
             for input_tag in form_details["inputs"]:
                 if input_tag["value"] or input_tag["type"] == "hidden":
-                    # any input form that has some value or hidden,
-                    # just use it in the form body
                     try:
                         data[input_tag["name"]] = input_tag["value"] + payload
                     except:
                         pass
                 elif input_tag["type"] != "submit":
-                    # all others except submit, use some junk data with special character
                     data[input_tag["name"]] = f"test{payload}"
 
-            # join the url with the action (form request URL)
             url = urljoin(url, form_details["action"])
             if form_details["method"] == "post":
                 res = s.post(url, data=data)
--- source/tools/self_made/XSS_scan.py
+++ source/tools/self_made/XSS_scan.py
@@ -5,53 +5,67 @@ import sys
 import re 
 import urllib.parse
 
-#06/09/2023 : CODE Can cai thien them mot so phuong dien de chen tim ra dia file (can cai tien tu dong 110 tro di)
+#06/09/2023 : CODE Can cai thien them tinh nang check submit selection
 #maker : RBKING
 
 s = requests.Session()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/117.0.5938.92"
 
-class LFI_check:
-
+class XSS:
+ 
   def __init__(self, url):
      self.url = url
+# login_payload = {
+#     "username": "admin",
+#     "password": "password",
+#     "Login": "Login",
+# }
+# # change URL to the login page of your DVWA login URL
+# login_url = "http://192.168.168.105/dvwa/login.php"
+
+# # login
+# r = s.get(login_url)
+# token = re.search("user_token'\s*value='(.*?)'", r.text).group(1)
+# login_payload['user_token'] = token
+# s.post(login_url, data=login_payload)
+
+
   # ---------------------------------------------------------------
-  #DEF SCAN WEBSITE
+
   forms = []
-  def scan_website(url):
+  def scan_website(self, url):
+    soup = bs(s.get(url).content, "html.parser")
     results = {
-      "lif": []
+      "xss": []
     }
     urls = urlparse(url)
     global forms
     forms = bs(s.get(url).content, "html.parser").find_all("form")
 
-    # Scan LFI
-    if self.check_lfi(url):
-      results["lfi"].append({
+    # Scan XSS
+    if self.check_xss(url):
+      results["xss"].append({
         "url": url,
-        "details": "[+] Local File Injection detected"
+        "details": "[+] Cross Site Scripting detected"
       })
 
     return results
+
+  # ---------------------------------
 
   def get_all_forms(self, url):
     soup = bs(s.get(url).content, "html.parser")
     return soup.find_all("form")
 
-  # Get form details
   def get_form_details(self, form):
     details = {}
-    # Get the form action (target url)
+
     try:
         action = form.attrs.get("action").lower()
     except:
         action = None
-
-    # get the form method (POST, GET, etc.)
     method = form.attrs.get("method", "get").lower()
 
-    # get all the input details such as type and name
     inputs = []
     for input_tag in form.find_all("input"):
         input_type = input_tag.attrs.get("type", "text")
@@ -59,59 +73,58 @@ class LFI_check:
         input_value = input_tag.attrs.get("value", "")
         inputs.append({"type": input_type, "name": input_name, "value": input_value})
 
-    # put everything to the resulting dictionary
     details["action"] = action
     details["method"] = method
     details["inputs"] = inputs
     return details
 
-  # Scan LFi function
-  def check_lfi(self, url):
-    with open('lfi_payloads.txt') as f:
-      lfi_payloads = f.read().splitlines()
-    
-    print("\n[+] Checking LFI")
+  # ---------------------------------------------------------------------
 
-    for payload in lfi_payloads:
-      encoded_payload = urllib.parse.quote(payload)
-      new_url = f"{url}?page={encoded_payload}"
+  def check_xss(self, url):
+    with open('xss_payload.txt') as f:
+      xss_payloads = f.read().splitlines()
+
+    print("\n[+] Checking XSS")
+
+    for payload in xss_payloads:
+      encoded_payload = urllib.parse.quote(payload) 
+      new_url = f"{url}?q={encoded_payload}"
 
       print("[!] Trying", new_url)
       res = s.get(new_url)
 
-      if re.search(rb"root:x:0:0", res.content):
-        print("[+] LFI vulnerability detected, link:", new_url)
+      if payload in res.text:
+        print("[+] XSS vulnerability detected, link:", new_url)
         return True
 
-    # Check from all form
     forms = self.get_all_forms(url)
-    print(f"[+] Detected {len(forms)} forms on {url}.")
+    print(f"[+] Detected {len(forms)} forms on {url}, form found: {forms}\n")
 
     for form in forms:
       form_details = self.get_form_details(form)
 
-      for payload in lfi_payloads:
+      for payload in xss_payloads:
         data = {}
 
         for input_tag in form_details["inputs"]:
           if input_tag["value"] or input_tag["type"] == "hidden":
             try:
-              data[input_tag["name"]] = input_tag["value"] + payload  
+              data[input_tag["name"]] = input_tag["value"] + payload
             except:
               pass
           elif input_tag["type"] != "submit":
             data[input_tag["name"]] = payload
-
+        
         url = urljoin(url, form_details["action"])
         if form_details["method"] == "post":
           res = s.post(url, data=data)
         elif form_details["method"] == "get":
           res = s.get(url, params=data)
 
-        if re.search(rb"root:x:0:0", res.content):
-          print("[+] LFI vulnerability detected, link:", url)
+        if payload in res.text:
+          print("[+] XSS vulnerability detected, link:", url)
           return True
 
-    print("[+] Check LFI done")    
+    print("[+] Check XSS done")
 
     return False
