44f2eca3e68ae8d7c1b99125f996ee908c86f4b8
vdh1612 <125654739+vdh1612@users.noreply.github.com>
2023-10-25 21:13:12 +0700

    path traversal

--- source/tools/self_made/path_traversal/path_traversal.py
+++ source/tools/self_made/path_traversal/path_traversal.py
@@ -1,81 +1,56 @@
 import requests
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urlparse, urljoin
-import sys
-import re 
+import re
 import urllib.parse
-
+import bson
 
 s = requests.Session()
 s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/117.0.5938.92"
 
+
 class PathTraversal:
 
-  def __init__(self, url):
-     self.url = url
+    def __init__(self, url):
+        self.url = url
 
-  forms = []
-  def scan_website(self):
-    results = {
-      "path_traversal": []
-    }
-    urls = urlparse(self.url)
-    global forms
-    forms = bs(s.get(self.url).content, "html.parser").find_all("form")
+    def scan_website(self):
+        results = {
+            "path_traversal": []
+        }
 
-    if self.check_path_traversal():
-      results["path_traversal"].append({
-        "url": self.url,
-        "details": "[+] path traversal detected"
-      })
+        if self.check_path_traversal():
+            results["path_traversal"].append({
+                "url": self.url,
+                "details": "[+] path traversal detected"
+            })
+        else:
+            results["path_traversal"].append({
+                "url": self.url,
+                "details": "[-] path traversal not detected"
+            })
 
-    return results
+        return results
 
-  def get_all_forms(self):
-    soup = bs(s.get(self.url).content, "html.parser")
-    return soup.find_all("form")
+    def check_path_traversal(self):
+        with open(f'{__file__}\\..\\path_traversal_payload.txt') as f:
+            path_traversal_payloads = f.read().splitlines()
 
-  def get_form_details(self, form):
-    details = {}
+        print("\n[+] Checking path traversal")
 
-    try:
-        action = form.attrs.get("action").lower()
-    except:
-        action = None
+        for payload in path_traversal_payloads:
+            encoded_payload = urllib.parse.quote(payload)
+            new_url = f"{self.url}?file_name={encoded_payload}"
 
-    method = form.attrs.get("method", "get").lower()
+            print("[!] Trying", new_url)
+            res = s.get(new_url)
 
-    inputs = []
-    for input_tag in form.find_all("input"):
-        input_type = input_tag.attrs.get("type", "text")
-        input_name = input_tag.attrs.get("name")
-        input_value = input_tag.attrs.get("value", "")
-        inputs.append({"type": input_type, "name": input_name, "value": input_value})
+            if re.search(rb"root:x:0:0", res.content):
+                print("[+] Path Traversal vulnerability detected, link:", new_url)
+                return True
 
-    details["action"] = action
-    details["method"] = method
-    details["inputs"] = inputs
-    return details
+        print("[+] Check path traversal done")
 
-  def check_path_traversal(self):
-    with open(f'{__file__}\\..\\path_traversal_payload.txt') as f:
-      path_traversal_payloads = f.read().splitlines()
-    
-    print("\n[+] Checking path traversal")
+        return False
 
-    for payload in path_traversal_payloads:
-      encoded_payload = urllib.parse.quote(payload)
-      new_url = f"{self.url}?file_name={encoded_payload}"
-
-      print("[!] Trying", new_url)
-      res = s.get(new_url)
-
-      if re.search(rb"root:x:0:0", res.content):
-        print("[+] Path Traversal vulnerability detected, link:", new_url)
-        return True
-    print("[+] Check path traversal done")    
-
-    return False
 
 path_traversal = PathTraversal("http://localhost:8091/loadImage.php")
 path_traversal.scan_website()
