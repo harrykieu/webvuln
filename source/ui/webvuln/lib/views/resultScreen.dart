b2849ee7bb3f44d9a9f8f30fa143b4f1f5e10ce3
tanle1810 <102292325+datatype123@users.noreply.github.com>
2023-11-07 18:42:29 +0700

    add new change file resultScreen.dart

--- source/ui/webvuln/lib/views/resultScreen.dart
+++ source/ui/webvuln/lib/views/resultScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:webvuln/items/infoTableError.dart';
+// import 'package:webvuln/items/infoTableXSSError.dart';
 import 'package:webvuln/main.dart';
 import 'package:webvuln/items/drawer.dart';
 import 'package:get/get.dart';
@@ -15,6 +15,13 @@ class resultScreen extends StatefulWidget {
 }
 
 class _resultScreenState extends State<resultScreen> {
+  late Widget table;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    table = tableSQLerror();
+  }
   @override
   Widget build(BuildContext context) {
     bool _isVisibled = true;
@@ -49,7 +56,6 @@ class _resultScreenState extends State<resultScreen> {
       ]),
       DataRow(cells: [DataCell(Text('30%')), DataCell(Text('RCE error'))])
     ];
-
     Get.testMode = true;
     return Scaffold(
         appBar: AppBar(
@@ -58,6 +64,9 @@ class _resultScreenState extends State<resultScreen> {
             style: textStyle,
             textAlign: TextAlign.center,
           ),
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.question_mark_rounded))
+          ],
           leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
         ),
         body: SingleChildScrollView(
@@ -75,7 +84,47 @@ class _resultScreenState extends State<resultScreen> {
                       color: Colors.black),
                 ),
               ),
-              tableError(),
+              Container(
+                width: double.infinity,
+                height: 100,
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 50,
+                      // padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+                      child: ElevatedButton(onPressed: (){
+                        setState(() {
+                          table = tableXSSError();
+                        });
+                      }, child: Text('XSS error',style:GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black)),
+                      style:ButtonStyle(
+                        backgroundColor: MaterialStateProperty<>.all(
+                          LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [Colors.blue, Colors.green],
+                          ) as Linear,
+                          )
+                      ), )
+                    ),
+                    Container(
+                      width: 200,
+                      height: 50,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+                      child: ElevatedButton(onPressed: (){
+                        setState(() {
+                          table = tableSQLerror();
+                        });
+                      }, child: Text('SQL injection',style:GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black)), )
+                    ),
+                  ],
+                ) ,
+              ),
+              table,
               SizedBox(
                 height: 10,
               ),
@@ -105,35 +154,100 @@ class _resultScreenState extends State<resultScreen> {
                   )
                 ],
               )
-              // infoTableError()
+              // infoTableXSSError()
             ]),
           ),
         ));
   }
 }
 
-class tableError extends StatefulWidget {
-  const tableError({super.key});
+class tableXSSError extends StatefulWidget {
+  const tableXSSError({super.key});
 
   @override
-  State<tableError> createState() => _tableErrorState();
+  State<tableXSSError> createState() => _tableXSSErrorState();
 }
 
-class _tableErrorState extends State<tableError> {
+class _tableXSSErrorState extends State<tableXSSError> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     List<DataColumn> dataColumns = [
-      DataColumn(label: Text("Serverity")),
-      DataColumn(label: Text("Type")),
-      DataColumn(label: Text('Vulnerabilities'))
+      DataColumn(label: Text("Serverity",style: GoogleFonts.montserrat(fontSize: 20,fontWeight: FontWeight.bold))),
+      DataColumn(label: Text("Type",style: GoogleFonts.montserrat(fontSize: 20,fontWeight: FontWeight.bold))),
+      DataColumn(label: Text('Vulnerabilities',style: GoogleFonts.montserrat(fontSize: 20,fontWeight: FontWeight.bold)))
     ];
-    final List<Map<String, dynamic>> data = {};
+    final List<Map<String, dynamic>> data = [
+        {'Serverity': Icons.warning_rounded, 'Type': 'XSS error', 'Vulnerabilities': 'google.com'},
+        {'Serverity':Icons.warning_rounded, 'Type': 'XSS error', 'Vulnerabilities': 'google.com'},
+        {'Serverity': Icons.warning_rounded, 'Type': 'XSS error', 'Vulnerabilities': 'google.com'},
+        {'Serverity': Icons.warning_rounded, 'Type': 'XSS error', 'Vulnerabilities': 'google.com'},
+        {'Serverity': Icons.warning_rounded, 'Type': 'XSS error', 'Vulnerabilities': 'google.com'},
+      ];
     return Container(
       width: screenWidth - 500,
       height: 500,
-      child: DataTable(columns: dataColumns, rows: dataRows),
+      decoration: BoxDecoration(
+        color: Color(0xFFD9D9D9),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black45,
+            blurRadius: 2,
+            offset: Offset(0, 4)
+          )
+        ]
+      ),
+      child: DataTable(columns: dataColumns, rows:data.map((item) =>
+        DataRow(cells: [
+          DataCell(Icon(item['Serverity'])),
+          DataCell(Text(item['Type'])),
+          DataCell(Text(item['Vulnerabilities']))
+        ]) ).toList() ),
+    );
+  }
+}
+
+class tableSQLerror extends StatefulWidget {
+  const tableSQLerror({super.key});
+
+  @override
+  State<tableSQLerror> createState() => _tableSQLerrorState();
+}
+
+class _tableSQLerrorState extends State<tableSQLerror> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    final List<DataColumn> dataColumns =[
+      DataColumn(label: Text('Serverity',style: GoogleFonts.montserrat(fontSize: 24,fontWeight: FontWeight.bold))),
+      DataColumn(label: Text('Type',style: GoogleFonts.montserrat(fontSize: 24,fontWeight: FontWeight.bold))),
+      DataColumn(label: Text('Vulnerabilities',style: GoogleFonts.montserrat(fontSize: 24,fontWeight: FontWeight.bold)))
+    ];
+    final List<Map<String,dynamic>> dataRows =[
+      {'Serverity':Icons.warning_rounded,'Type':'SQL Injection','Vulnerabilities':'wikipedia.com'},
+      {'Serverity':Icons.warning_rounded,'Type':'SQL Injection','Vulnerabilities':'wikipedia.com'},
+      {'Serverity':Icons.warning_rounded,'Type':'SQL Injection','Vulnerabilities':'wikipedia.com'},
+      {'Serverity':Icons.warning_rounded,'Type':'SQL Injection','Vulnerabilities':'wikipedia.com'},
+      {'Serverity':Icons.warning_rounded,'Type':'SQL Injection','Vulnerabilities':'wikipedia.com'}
+    ];
+    return Container(
+      width: screenWidth - 500,
+      height: 500,
+      decoration: BoxDecoration(
+        color: Color(0xFFD9D9D9),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(color: Colors.black45,offset: Offset(0, 4),blurRadius: 2)
+        ]
+      ),
+      child: DataTable(columns:dataColumns,rows: dataRows.map((item) => DataRow(cells: [
+        DataCell(Icon(item['Serverity'])),
+        DataCell(Text(item['Type'])),
+        DataCell(Text(item['Vulnerabilities']))
+      ]) ).toList(),),
     );
   }
 }
