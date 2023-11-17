10f418e750025857e3361b3671616a74b778d3c7
harrykieu <huyhai321@gmail.com>
2023-11-17 22:19:04 +0700

    Edit backend

--- source/core/backend.py
+++ source/core/backend.py
@@ -118,7 +118,10 @@ class WebVuln:
         }
         ```
         """
-
+        # Remove all other log files
+        for file in Path(f"{ROOTPATH}/logs").iterdir():
+            if file.is_file() and file.name != "log.txt":
+                os.remove(file)
         commands = []
         jsonFiles = []
         dirURL = {}
@@ -587,7 +590,7 @@ class WebVuln:
                 listResult = []
                 for item in cursor:
                     listResult.append(item)
-                return str(listResult)
+                return listResult
             else:
                 utils.log(
                     "[backend.py-getScanResult] Error: Invalid JSON object", "ERROR"
