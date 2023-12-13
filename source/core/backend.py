1b02aaad0327e391eed81806edf33a8063f2d48d
Trí Tiến <113023958+rumblelinux@users.noreply.github.com>
2023-12-14 01:04:40 +0700

    Update backend.py

--- source/core/backend.py
+++ source/core/backend.py
@@ -15,8 +15,6 @@ from source.tools.self_made.xss.scan_xss import XSS
 from source.tools.self_made.idor.scan_idor import IDOR
 from source.core.calSeverity import calculateWebsiteSafetyRate
 
-import xml.etree.ElementTree as ET
-import pdfkit
 
 ROOTPATH = Path(__file__).parent.parent.parent
 MODULES = [
@@ -690,71 +688,3 @@ class WebVuln:
                 if self.__debug:
                     print("[backend.py-getScanResult] Error: Invalid JSON object")
                 return "Failed"
-
- # TODO: retrieve path from GUI, save at that location, return "Success" or "Failed" => SOLVED 09/12/2023
-class ReportGenerator:
-    def __init__(self, scan_results, file_path):
-        self.scan_results = scan_results
-        self.file_path = file_path
-
-    def generate_json(self):
-        file_name = os.path.join(self.file_path, 'report.json')
-        with open(file_name, 'w') as f:
-            json.dump(self.scan_results, f, indent=4)
-        return "Success"
-
-    def generate_xml(self):
-        root = ET.Element('scan_results')
-        for result in self.scan_results:
-            domain = ET.SubElement(root, 'domain')
-            ET.SubElement(domain, 'name').text = result['domain'] 
-            ET.SubElement(domain, 'scan_date').text = str(result['scanDate'])
-            
-            vulns = ET.SubElement(domain, 'vulnerabilities')
-            for vuln in result['vulnerabilities']:
-                v = ET.SubElement(vulns, 'vulnerability')
-                ET.SubElement(v, 'type').text = vuln['type']
-                ET.SubElement(v, 'severity').text = vuln['severity']
-                ET.SubElement(v, 'payload').text = str(vuln['payload'])
-                ET.SubElement(v, 'logs').text = vuln['logs']
-
-        tree = ET.ElementTree(root)
-        file_name = os.path.join(self.file_path, 'report.xml') 
-        tree.write(file_name)
-        
-        return "Success"
-
-    def generate_pdf(self):    
-        pdf_content = '''
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <style>
-        body {
-            background-color: white; 
-            font-family: Arial, sans-serif;
-        }
-        </style>
-        </head>
-        <body>
-        '''
-        
-        for result in self.scan_results:
-            pdf_content += f'<h1>Domain: {result["domain"]}</h1>'
-            pdf_content += f'<p>Scan Date: {result["scanDate"]}</p>'
-            pdf_content += f'<p>Vulnerabilities Detected: {result["numVuln"]}</p>'
-            
-            pdf_content += '<h2>Vulnerabilities:</h2>'
-            pdf_content += '<ul>'
-            for vuln in result['vulnerabilities']:
-                pdf_content += f'<li>{vuln["type"]} ({vuln["severity"]}):'
-                pdf_content += f'<p>Payload: {vuln["payload"]}</p>'
-                pdf_content += f'<p>Logs: {vuln["logs"]}</p></li>'
-            pdf_content += '</ul>'
-            
-        pdf_content += '</body></html>'
-        
-        file_name = os.path.join(self.file_path, 'report.pdf')
-        pdfkit.from_string(pdf_content, file_name)
-        
-        return "Success"
